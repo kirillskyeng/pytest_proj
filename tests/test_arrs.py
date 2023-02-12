8069baa57cbd5d5034dee2408b0d017c05c061b0
irinaloskutova <l.irinka@mail.ru>
2023-02-12 10:57:26 +0500

    исправлены 2 теста

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,8 +2,8 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], -1, "test") == "test"
 
 
 def test_slice():
