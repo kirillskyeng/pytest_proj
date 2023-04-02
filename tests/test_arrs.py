8b0abcaaca074a2d55a72f4fb46fac01bd2de417
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-04-02 21:50:19 +0300

    commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,7 @@ def test_get():
     assert arrs.get([4, 5, 6], 2, "test") == 6
     assert arrs.get([1, 2, 3], -1, "test") == "test"
 
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
