b59609984fc95d20ea5f28689b48d37c01b776bd
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-04-02 21:49:15 +0300

    commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,9 +3,8 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([1, 2, 3], -1, "test") == "test"
     assert arrs.get([4, 5, 6], 2, "test") == 6
-
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
