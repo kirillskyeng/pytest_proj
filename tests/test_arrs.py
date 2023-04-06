4d67a28de37098a29e072cd4f63ba6ca466ab2ce
Anton Parshikov <tosha1703@gmail.com>
2023-04-06 21:07:48 +0300

    add changes in tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,8 +5,12 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get(["test", 1, 2], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -10) == []
