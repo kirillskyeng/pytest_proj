77062a1b78b07f9d6b09b2f84ae3056322fcdcda
Petr Davydov <Davydov.atc@yandex.ru>
2023-04-28 15:45:37 +0300

    change test_get and func 'get', now test passed

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,9 +2,15 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
-
+    from utils.arrs import get
+    assert get([1, 2, 3], 0) == 1
+    assert get([1, 2, 3], 1) == 2
+    assert get([1, 2, 3], 2) == 3
+    assert get([1, 2, 3], 3, "default") == "default"
+    assert get([1, 2, 3], -1, "default") == "default"
+    assert get([], 0, "default") == "default"
+    assert get([1, 2, 3], 4) == None
+    assert get([1, 2, 3], -1) == None
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
