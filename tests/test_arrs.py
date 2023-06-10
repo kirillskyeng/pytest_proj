d4f36341183f79f4807fa99766d9b7d613b1139f
Sergey <kapetanvodichka@gmail.com>
2023-06-11 03:01:43 +0500

    fix 2 tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,12 @@
+import pytest
+
+
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], -1, "test") == "test"
 
 
 def test_slice():
