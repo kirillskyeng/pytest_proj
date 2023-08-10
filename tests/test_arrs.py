118fc2c4f6bb396ad61db49fd0fbe33aea385c8c
vp <mr.r9ch@yandex.ru>
2023-08-10 12:48:49 +0300

    test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,9 @@
 from utils import arrs
 
-
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+import pytest
+def test2_get():
+    with pytest.raises(IndexError):
+        assert arrs.get([], 0, "test")
 
 
 def test_slice():
